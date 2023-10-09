e37d8bf4f670f1e161aa4d8f03d62e5a1ea2b184
QQ47 <dangdiemquynh47@gmail.com>
2023-10-09 18:01:46 +0700

    update

--- assignment-2/src/demo.jsx
+++ assignment-2/src/demo.jsx
@@ -1,16 +1,10 @@
 import { useState } from "react";
 
 const Demo = () => {
-  const [text, setText] = useState("");
   const [textName, setName] = useState("");
   const [textAuthor, setAuthor] = useState("");
   const [textTopic, setTopic] = useState("");
 
-  const onChange = (e) => {
-    setText(e.target.value);
-    console.log(text);
-
-  };
   const onChangeName = (e) => {
     setName(e.target.value);
     console.log(e.target.value);
@@ -23,170 +17,137 @@ const Demo = () => {
     setTopic(e.target.value);
   }
 
-  const [list, setList] = useState(["Task 1"]);
-  const createTask = () => {
-    setList([...list, text]);
-    setText("");
-  };
-
+  const [list, setList] = useState([{
+    name: "quynh",
+    author: "cute",
+    topic: "phomaique"
+  }]);
   const deleteTask = (index) => {
-    const newList = [...list];
-    newList.splice(index, 1);
-    setList(newList);
-  };
-
-  const [edit, setEdit] = useState(-1);
-
-  const onUpdate = () => {
-    const newList = [...list];
-    newList[edit] = text;
-    setList(newList);
-    setEdit(-1);
-    setText("");
-  };
-
-  const onEdit = (index) => {
-    setText(list[index]);
-    setEdit(index);
-  };
-
-  const cancelUpdate = () => {
-    setText("");
-    setEdit(-1);
+    list.splice(index, 1)
+    setList([...list])
   };
   const [active, setActive] = useState(false);
   console.log(active);
+  const saveObj = () => {
+    const obj = {
+      name: textName,
+      author: textAuthor,
+      topic: textTopic
+    }
+    list.push(obj)
+    setList([...list])
+    setName("")
+    setAuthor("")
+    setTopic("")
+    console.log(list)
+  }
 
+  const closeModal = () => {
+    setActive(!active)
+  }
   return (
-    <div className="pt-10">
-      <div className="">
-        <input
-          type="text"
-          value={text}
-          onChange={onChange}
-          placeholder="nhap task name"
+    <div className="">
+      <div className="flex items-center gap-3 bg-white py-4 px-10">
+        <p>BookStore</p>
+        <img
+          src="https://nguoinoitieng.tv/images/nnt/98/0/bc38.jpg"
+          alt=""
+          className="w-8 h-8 ml-auto rounded-full"
         />
-        <button onClick={edit !== -1 ? onUpdate : createTask}>
-          {edit !== -1 ? "update" : "add task"}
-        </button>
-        {edit !== -1 && (
-          <button onClick={() => cancelUpdate()}>cancel update</button>
-        )}
+        <p>Mina Myoui</p>
       </div>
-      {list.map((item, index) => {
-        return (
-          <div className="">
-            <div className="">{item}</div>
-            <button onClick={() => onEdit(index)}>gekki</button>
-            <button onClick={() => deleteTask(index)}>delete</button>
-          </div>
-        );
-      })}
-
-      <div className="p-10">
-        <div className="flex items-center gap-3 bg-white py-4">
-          <p>BookStore</p>
-          <img
-            src="https://nguoinoitieng.tv/images/nnt/98/0/bc38.jpg"
-            alt=""
-            className="w-8 h-8 ml-auto rounded-full"
+      <div className="h-[1px] w-full bg-neutral-400 shadow-md"></div>
+      <div className="bg-neutral-100 px-5">
+        <div className="flex items-center pt-10 mb-6">
+          <input
+            type="text"
+            placeholder="Search"
+            className="px-3 py-1 border-[1px] border-solid border-neutral-300 rounded-sm  mr-3"
           />
-          <p>Mina Myoui</p>
+          <button className="text-white bg-red-400 px-3 py-1 ml-auto " onClick={() => setActive(!active)}>
+            Add book
+          </button>
+
         </div>
-        <div className="h-[1px] w-full bg-neutral-400 shadow-md"></div>
-        <div className="bg-neutral-100 px-5">
-          <div className="flex items-center pt-10 ">
-            <input
-              type="text"
-              placeholder="Search"
-              className="px-3 py-1 border-[1px] border-solid border-neutral-300 rounded-sm"
-            />
-            <button className="text-white bg-red-500 px-3 py-1 rounded-sm ml-auto" onClick={()=>setActive(!active)}>
-              Add book
-            </button>
-            
-          </div>
-          <div className={" border-[1px] border-solid border-neutral-400 w-[40%] mx-auto rounded-sm p-10 " + (active === true ? "" : "hidden")}>
-            <div className="flex items-center">
-              <p>Add Book</p>
-              <i className="fa-solid fa-xmark w-10 h-10 stroke-neutral-700 fill-neutral-600"></i>
-            </div>
-            <div className="pt-10">
-              <div className="">
-                <p>Name</p>
-                <input
-                  type="text"
-                  name=""
-                  id=""
-                  placeholder="Enter Name"
-                  value={textName}
-                  onChange={onChangeName}
-                  className="px-3 py-1 border-[1px] border-solid border-neutral-300 rounded-sm"
-                />
-              </div>
-              <div className="">
-                <p>Author</p>
-                <input
-                  type="text"
-                  name=""
-                  id=""
-                  placeholder="Enter Author"
-                  value={textAuthor}
-                  onChange={onChangeAuthor}
-                  className="px-3 py-1 border-[1px] border-solid border-neutral-300 rounded-sm"
-                />
-              </div>{" "}
-              <div className="">
-                <p>Topic</p>
-                <input
-                  type="text"
-                  name=""
-                  id=""
-                  placeholder="Enter Topic"
-                  value={textTopic}
-                  onChange={onChangeTopic}
-                  className="px-3 py-1 border-[1px] border-solid border-neutral-300 rounded-sm"
-                />
+          <div className={"modal w-full h-full" + (active === true ? "" : " hidden")}>
+            <div className="fixed flex items-center justify-center top-0 left-0 w-screen h-screen ">
+              <div className="absolute z-0 bg-black/50 top-0 left-0 w-screen h-screen" onClick={closeModal}></div>
+              <div className="w-fit relative z-10  p-10 bg-white rounded-xl">
+                <div className="flex items-center h-10">
+                  <p className="pb-10 font-bold text-3xl">Add book</p>
+                  <i className="fa-solid fa-xmark ml-auto" onClick={closeModal}></i>
+                </div>
+                <div className="mb-10">
+                  <p>Name</p>
+                  <input
+                    type="text"
+                    name=""
+                    id=""
+                    placeholder="Enter Name"
+                    value={textName}
+                    onChange={onChangeName}
+                    className="px-3 py-1 border-[1px] border-solid border-neutral-300 rounded-sm"
+                  />
+                </div>
+                <div className="mb-10">
+                  <p>Author</p>
+                  <input
+                    type="text"
+                    name=""
+                    id=""
+                    placeholder="Enter Author"
+                    value={textAuthor}
+                    onChange={onChangeAuthor}
+                    className="px-3 py-1 border-[1px] border-solid border-neutral-300 rounded-sm"
+                  />
+                </div>
+                <div className="mb-10">
+                  <p>Topic</p>
+                  <input
+                    type="text"
+                    name=""
+                    id=""
+                    placeholder="Enter Topic"
+                    value={textTopic}
+                    onChange={onChangeTopic}
+                    className="px-3 py-1 border-[1px] border-solid border-neutral-300 rounded-sm"
+                  />
+                </div>
+                <div class="flex">
+                <button className="bg-red-400 text-white px-3 py-1 rounded-sm ml-auto" onClick={saveObj}>
+            Create
+          </button>
+
+                </div>
               </div>
             </div>
-            <button className="bg-red-400 text-white px-3 py-1 rounded-sm ml-auto">
-              Create
-            </button>
           </div>
-          <div class=" px-20">
-            <table class="border-collapse border border-slate-400 w-full">
-              <thead>
-                <tr>
-                  <th class="border border-slate-300 w-5/12">Name</th>
-                  <th class="border border-slate-300 w-3/12">Author</th>
-                  <th class="border border-slate-300 w-2/12">Topic</th>
-                  <th class="border border-slate-300 w-2/12">Action</th>
-                </tr>
-              </thead>
-              <tbody>
-                <tr>
-                  <td class="border border-slate-300">Refactoring</td>
-                  <td class="border border-slate-300"> Martin Fowler</td>
-                  <td class="border border-slate-300">Programming</td>
-                  <td></td>
-                </tr>
-                <tr>
-                  <td class="border border-slate-300">
-                    Designing Data-Intensive Applications
-                  </td>
-                  <td class="border border-slate-300">Martin Kleppmann</td>
-                  <td class="border border-slate-300">Database</td>
-                  <td></td>
-                </tr>
-                <tr>
-                  <td class="border border-slate-300">The Phoenix Project</td>
-                  <td class="border border-slate-300">Gene Kim</td>
-                  <td class="border border-slate-300">DevOps</td>
-                  <td></td>
-                </tr>
-              </tbody>
-            </table>
-          </div>
+        <div className=" px-20">
+          <table className="border-collapse border border-slate-400 w-full">
+            <thead>
+              <tr>
+                <th className="border border-slate-300 w-5/12">Name</th>
+                <th className="border border-slate-300 w-3/12">Author</th>
+                <th className="border border-slate-300 w-2/12">Topic</th>
+                <th className="border border-slate-300 w-2/12">Action</th>
+              </tr>
+            </thead>
+            <tbody>
+              {list.map((item, index) => {
+                return (
+                  <tr>
+                    <td className="border border-slate-300">{item.name}</td>
+                    <td className="border border-slate-300">{item.author}</td>
+                    <td className="border border-slate-300">{item.topic}</td>
+                    <td className="border border-slate-300 cursor-pointer underline text-red-600" onClick={() => deleteTask(index)}>Delete</td>
+                  </tr>
+                )
+              })}
+
+
+
+            </tbody>
+          </table>
         </div>
       </div>
     </div>
